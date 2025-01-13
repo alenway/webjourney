21d83a37b3229a33259cd6730a0ba2b537cdfe3b
Narendra Samtani <alenway97@gmail.com>
2025-01-13 23:16:17 +0530

    learning javascript basics

---
+++ topics/js/foundation/part1/datatypes.js
@@ -0,0 +1,26 @@
+console.log("hello");
+// single line comments
+/*
+multi line comment
+
+string
+number
+boolean
+bigint
+
+undefined
+null
+objects
+
+symbol
+*/
+
+// var score = 10
+let score = 10;
+let name = "name"
+let is_true = false
+const unchange = 32
+
+// objects
+let type = ["single", "multi", "solo"]
+let user = {firstname: "nitin", lastname: "soni"}
