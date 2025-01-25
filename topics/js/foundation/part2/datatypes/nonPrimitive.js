d14e824983ea44a1a345fd1d96986568739272a6
Narendra Samtani <alenway97@gmail.com>
2025-01-25 22:32:04 +0530

    primitive and non primitive datatype

---
+++ topics/js/foundation/part2/datatypes/nonPrimitive.js
@@ -0,0 +1,11 @@
+let username = {
+    firstname: "nitin",
+    isLoggedin: true,
+}
+
+const myFirstName = "narendra"
+myFirstName = "nitin"
+
+console.log(username);
+console.log(typeof username);
+
---
+++ topics/js/foundation/part2/datatypes/primitive.js
@@ -0,0 +1,35 @@
+// number
+let balance = 120
+let anotherbalance = new Number(120)
+// console.log(typeof(balance));
+// console.log(anotherbalance.valueOf());
+
+// console.log(typeof(balance));
+// console.log(typeof(anotherbalance));
+
+// boolean
+let isActive = true
+let isReallyActive = new Boolean(true)
+
+// null and undefined
+let firstname = null
+let lastname = undefined
+// console.log(firstname);
+// console.log(lastname);
+
+// string
+let mystring = "hello"
+let mystringone = 'hola'
+let username = 'nitin'
+
+// string intropulation
+let greetmessage = `hello ${username}`
+let demoone = `value of sum ${2 + 2}`
+// console.log(greetmessage);
+// console.log(demoone);
+
+// symbol
+let sm1 = Symbol("narendra")
+let sm2 = Symbol("narendra")
+
+console.log(sm1 === sm2);
