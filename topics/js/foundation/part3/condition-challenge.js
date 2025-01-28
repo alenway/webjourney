29b1c16172a28d9e9ac9efb22bed9aebbd13609c
Narendra Samtani <alenway97@gmail.com>
2025-01-28 18:42:09 +0530

    conditional challenges

---
+++ topics/js/foundation/part3/condition-challenge.js
@@ -0,0 +1,43 @@
+// checking if a number is greater than another number
+let num1 = 5
+let num2 = 8
+
+if(num1 > num2){
+  console.log(`number one is greater than number two ${num1}`)
+} else{
+  console.log(`number two is greater than number one ${num2}`)
+}
+
+// checking if a string is equal to another string
+let username = "nitin"
+let anotherUser = "nitin"
+
+if (username == anotherUser){
+  console.log("pick another username")
+} else{
+  console.log("you can pick the username")
+}
+
+let score = 44
+if (typeof score === 'number'){
+  console.log("yes, this is a number")
+} else {
+  console.log("no, this is not a number")
+}
+
+let isTready = false
+
+if(isTready){
+  console.log("this is ready")
+} else{
+  console.log("this is not ready")
+}
+
+// checking if an array is empty or not
+let itme = []
+console.log(itme.length)
+if(item.length){
+  console.log("array is empty")
+} else {
+  console.log("array is not empty")
+}
