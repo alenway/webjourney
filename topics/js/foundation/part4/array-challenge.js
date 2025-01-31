72b1871580a926712f19b4689f7fdc8fb7b70232
Narendra Samtani <alenway97@gmail.com>
2025-01-31 19:05:06 +0530

    working on js challenges

---
+++ topics/js/foundation/part4/array-challenge.js
@@ -0,0 +1,14 @@
+/*
+1.declare an array named 'teaflavours' that contains the string '"green tea", "black tea", and "oolong tea"'.
+Access the first element of the array and store it in a variable named "firsttea".
+*/
+
+let teaflavours = ["green tea", "black tea", "oolong tea"]
+
+// another way to declare the array in JS
+// let teaflovours = new Array("green tea", "black tea", "oolong tea")
+
+const firsttea = teaflavours[0]
+
+/*
+2.declare an array named 
